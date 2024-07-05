b475e150dfacbfe9c9387ab8f2a4356e9490caa4
Taylor <taylor_754@hotmail.com>
2024-07-05 16:26:44 -0400

    Need to add animation to the ticker

--- src/components/sports/ScoreTicker.jsx
+++ src/components/sports/ScoreTicker.jsx
@@ -19,13 +19,18 @@ import { prepScreens } from "../../controllers/scoreTickerController";
 import MLBScoreBox from "./MLBScoreBox";
 import "./scoreTicker.css";
 
+
 export const ScoreTicker = ({ mlb }) => {
 
+    //the actual data that is processed is memoized so that it does not get recomputed on re-render
     const processedMLBData = useMemo(() => { return processMLBData(mlb); }, [mlb])
     const screensData = useMemo(() => { return prepScreens({ mlb: processedMLBData }) }, [processedMLBData]);
 
+    //this is using state so that we can trigger the re-render
     const [activeScreen, setActiveScreen] = useState(0);
     const activeScreenData = useMemo(() => { return screensData[activeScreen] }, [activeScreen]);
+
+    //timer for changing the active screen
     setTimeout(() => {
         if (activeScreen === screensData.length - 1) {
             setActiveScreen(0);
@@ -35,8 +40,9 @@ export const ScoreTicker = ({ mlb }) => {
 
     if (!activeScreenData) return null;
 
+    //giving the score ticker a unique key means that it will be re-drawn on each re-render
     return (
-        <div className="score-ticker">
+        <div key={`score-ticker-active-screen-${activeScreen}`} className="score-ticker">
             <div className="league-name">{activeScreenData.league}</div>
             {activeScreenData.games.map((game) => { return <MLBScoreBox gameData={game} /> })}
         </div>)
